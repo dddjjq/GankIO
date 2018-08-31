51bf081a2396af95ffeb2fb3f2c1e9aeb576faeb
welson <dingyunlong1996@hotmail.com>
2018-08-31 20:38:32 +0800

    complete

--- app/src/main/java/com/dingyl/gankio/activity/ContentActivity.java
+++ app/src/main/java/com/dingyl/gankio/activity/ContentActivity.java
@@ -1,8 +1,12 @@
 package com.dingyl.gankio.activity;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.dingyl.gankio.Adapter.ContentListAdapter;
@@ -16,6 +20,7 @@ public class ContentActivity extends AppCompatActivity {
     private ArrayList<AndroidCategory.AndroidBean> androidBeanArrayList;
     private ListView contentList;
     private ContentListAdapter adapter;
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +40,37 @@ public class ContentActivity extends AppCompatActivity {
         if("intent_action_content".equals(intent.getAction())){
             androidBeanArrayList = (ArrayList<AndroidCategory.AndroidBean>)intent.getSerializableExtra("content_list");
         }
+        actionBar = getSupportActionBar();
+        //actionBar.setIcon(R.drawable.back);
+        actionBar.setTitle(androidBeanArrayList.get(0).getPublishedAt().substring(0,10));
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        contentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ContentActivity.this,WebActivity.class);
+                intent.putExtra("web_url",androidBeanArrayList.get(position).getUrl());
+                intent.putExtra("desc",androidBeanArrayList.get(position).getDesc());
+                startActivity(intent);
+            }
+        });
     }
 
     private void initListener(){
         adapter = new ContentListAdapter(this,androidBeanArrayList);
         contentList.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch (id){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
